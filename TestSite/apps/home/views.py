0f7fa0e06d098025c910b9cf624cc31a07ab822b
BogdanVR666 <bogdanvr666@gmail.com>
2021-08-14 22:25:40 +0300

    v0.0.0.6

--- TestSite/apps/home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Product
-
-def home(request):
-    profit_products = Product.objects.order_by('-price')
-    return render(request, 'home/index.html', {'profit_products':profit_products})
-
-def test(reqest):
-    pass
